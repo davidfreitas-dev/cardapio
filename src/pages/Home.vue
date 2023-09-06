94f62d84919b3f3f2ea5fbbef78e7ec43b69f5a7
David Freitas <davidfreitas.dev@outlook.com>
2023-09-06 15:59:41 -0300

    set relative path

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,7 @@
+<script setup>
+import HelloWorld from "@/components/HelloWorld.vue";
+</script>
+
 <template>
-  <h1 class="text-3xl font-bold">
-    Hello World!
-  </h1>
+  <HelloWorld />
 </template>
