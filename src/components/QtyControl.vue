b4bfe7699ec4c63a2318cd30c3e389cbab7fa3ad
David Freitas <davidfreitas.dev@outlook.com>
2023-10-30 15:00:40 -0300

    ajuste no componente de controle de quantidade

--- src/components/QtyControl.vue
+++ src/components/QtyControl.vue
@@ -1,39 +1,48 @@
 <script setup>
-import { computed } from 'vue';
+import { ref, watch, computed } from 'vue';
 import { PlusIcon, MinusIcon } from '@heroicons/vue/24/solid';
-
-const emit = defineEmits(['update:modelValue']);
 
 const props = defineProps({
   modelValue: { 
     type: Number, 
-    default: 0 
+    default: 1
   }
 });
 
+const quantity = ref(1);
+
+watch(
+  () => props.modelValue, 
+  () => {
+    quantity.value = props.modelValue;
+  }
+);
+
 const isDisabled = computed(() => {
-  return props.modelValue < 2;
+  return quantity.value < 2;
 });
 
+const emit = defineEmits(['update:modelValue']);
+
+const updateValue = (event) => {
+  emit('update:modelValue', event.target.value);
+};
+
 const decrement = () => {
-  const count = parseInt(props.modelValue) - 1;
+  const count = parseInt(quantity.value) - 1;
   emit('update:modelValue', count);
 };
 
 const increment = () => {
-  const count = parseInt(props.modelValue) + 1;
+  const count = parseInt(quantity.value) + 1;
   emit('update:modelValue', count);
-};
-
-const updateValue = (event) => {
-  emit('update:modelValue', event.target.value);
 };
 </script>
 
 <template>
   <div class="flex items-center">
     <button
-      class="flex items-center justify-center bg-primary disabled:bg-gray-200 disabled:text-secondary text-white transition-colors rounded-full h-6 w-6 disabled:cursor-not-allowed"
+      class="flex items-center justify-center bg-primary disabled:bg-gray-200 disabled:text-secondary text-white transition-colors rounded-lg h-7 w-7 disabled:cursor-not-allowed"
       :disabled="isDisabled"
       @click="decrement"
     >
@@ -48,7 +57,7 @@ const updateValue = (event) => {
     >
 
     <button
-      class="flex items-center justify-center bg-primary text-white rounded-full h-6 w-6"
+      class="flex items-center justify-center bg-primary text-white rounded-lg h-7 w-7"
       @click="increment"
     >
       <PlusIcon class="h-4 w-4" />
