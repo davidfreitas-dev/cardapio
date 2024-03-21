06fdd0c7441cd00a0c09346dc8a20581fe5e7cfb
David Freitas <davidfreitas.dev@outlook.com>
2024-03-21 16:26:07 -0300

    add action to plus button on product card

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,8 +1,8 @@
 <script setup>
-import { computed } from 'vue';
 import { useRouter } from 'vue-router';
 import { useStorage } from '@/use/useStorage';
 import { PlusIcon } from '@heroicons/vue/24/outline';
+import { useCartStore } from '@/stores/cart';
 
 const props = defineProps({
   item: {
@@ -23,8 +23,25 @@ const router = useRouter();
 
 const handleSelectItem = () => {
   setStorage('product', props.item);
-
   router.push('/item');
+};
+
+const cartStore = useCartStore();
+
+const handleAddToCart = () => {
+  const item = { ...props.item };
+  const cartProducts = cartStore.cart.products;
+  const index = cartProducts.findIndex((products) => products.id === item.id);
+
+  if (index >= 0) {
+    cartProducts[index].quantity += item.quantity;
+    cartProducts[index].additional = item.additional;
+    return;
+  } 
+  
+  item.quantity = item.quantity || 1;
+
+  cartStore.addToCart(item);
 };
 
 const { setStorage } = useStorage();
@@ -34,12 +51,12 @@ const { setStorage } = useStorage();
   <div
     class="card-container flex flex-col gap-2 w-full shadow-xl rounded-xl cursor-pointer p-3"
     :class="{'min-w-[40%] max-w-[40%] mb-8': slide}"
-    @click="handleSelectItem"
   >
     <img
       :src="getImageUrl(item)"
       :alt="`Imagem de ${item.description}`"
       class="card-img w-full h-auto rounded-lg"
+      @click="handleSelectItem"
     >
     <div class="card-content flex flex-col w-full">
       <div class="card-title font-bold text-sm text-font h-[40px]">
@@ -51,7 +68,7 @@ const { setStorage } = useStorage();
           {{ $filters.currencyBRL(item.price) }}
         </strong>
         
-        <button class="flex gap-1 text-primary bg-primary bg-opacity-20 font-semibold rounded-lg p-1.5">
+        <button @click="handleAddToCart" class="flex gap-1 text-primary bg-primary bg-opacity-20 font-semibold rounded-lg p-1.5">
           <PlusIcon class="h-4 w-4" />
         </button>
       </div>
